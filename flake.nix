6b9adba0fd05883fb71efd3f756813ee87bf9f50
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-06-04 17:32:08 +0300

    chore: Add home-manager config

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     hyprland.url = "github:hyprwm/Hyprland";
-   };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";   };
 
 
-  outputs = { self, nixpkgs, hyprland, ... }: 
+  outputs = { self, nixpkgs, hyprland, home-manager, ... }: 
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -22,9 +23,18 @@
               inherit system;
               modules = [ ./configuration.nix 
               hyprland.nixosModules.default
-              {programs.hyprland.enable = true;
+              {
+              programs.hyprland.enable = true;
               programs.hyprland.nvidiaPatches=true;
-              programs.hyprland.xwayland.enable=true;}];
+              programs.hyprland.xwayland.enable=true;
+              }
+              home-manager.nixosModules.home-manager
+              {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.kourosh = import ./home.nix;
+              }
+];
             };
           };
       };

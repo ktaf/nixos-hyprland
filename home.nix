1dd76caca38b735234f8b540e5fddc1821fd422f
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-06-04 17:49:22 +0300

    fix: home-manager init config

--- home.nix
+++ home.nix
@@ -1,11 +1,59 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "kourosh";
   home.homeDirectory = "/home/kourosh";
+  home.stateVersion = "23.05";
 
+	nixpkgs = {
+		config = {
+			allowUnfree = true;
+			allowUnfreePredicate = (_: true);
+		};
+	};
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  home.packages = with pkgs; [
+		bat
+		jq
+		tree
+		obsidian
+    vscode
+    slack
+    tdesktop
+    
+		];
+
+
+	home.sessionVariables = {
+		EDITOR="code";
+	};
+
+	programs.zsh = {
+		enable = true;
+		shellAliases = {
+			ll = "ls -la";
+			cat = "bat";
+		};
+		oh-my-zsh = {
+			enable = true;
+			plugins = [ "git" "python" "docker" ];
+			theme = "dpoggi";
+		};
+	};
+
+	programs.git = {
+			enable = true;
+			userName = "ktaf";
+			userEmail = "kouroshtaf@gmail.com";
+			extraConfig = {
+				init.defaultBranch = "main";
+				core.editor = "nano";
+				protocol.keybase.allow = "always";
+				pull.rebase = "false";
+				};
+}
 
   #Gtk 
     gtk = {
@@ -37,16 +85,4 @@
 
 
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }

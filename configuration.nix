21bd2dc41499c71607fe56ded6dc7de10f0fc17b
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-06-04 18:34:54 +0300

    prune few pacages

--- configuration.nix
+++ configuration.nix
@@ -362,11 +362,8 @@ programs.steam = {
   environment.systemPackages = with pkgs; [
  # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      vim
-     zig
      wget
-     killall
      git
-     neofetch
      gh
      #OVMFFull
      #qemu

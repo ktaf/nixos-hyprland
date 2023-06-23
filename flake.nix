5470ba1fde879ff8653281fe0e05178a8a471617
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-06-23 22:58:35 +0300

    chore: Add nix formatter

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@ description = "ktaf-nixos";
 
 inputs = {
   nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  hyprland.url = "github:hyprwm/Hyprland";
-  home-manager.url = "github:nix-community/home-manager/release-23.05";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  hyprland = {
+    url = "github:hyprwm/Hyprland";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  home-manager = {
+    url = "github:nix-community/home-managerrelease-23.05";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
@@ -43,20 +48,46 @@ outputs = { self, nixpkgs, hyprland, home-manager, treefmt-nix, ... }:
             home-manager.extraSpecialArgs = {inherit user;};
             home-manager.users.${user} = import ./modules/home.nix;
             }
-];
+            ];
           };
         };
-    };
-  formatter.x86_64-linux = treefmt-nix.lib.mkWrapper
-    nixpkgs.legacyPackages.x86_64-linux
-    {
-      projectRootFile = "flake.nix";
-      programs.nixpkgs-fmt.enable = true;
-      # Here you can specify the formatters to use
-      programs.terraform.enable = true;
-      # ...and options
-      programs.terraform.package = nixpkgs.terraform_1;
-    };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { config, inputs', pkgs, system, lib, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlays.default
+            ];
+          };
+        in
+        {
+          treefmt.config = {
+            inherit (config.flake-root) projectRootFile;
+            package = pkgs.treefmt;
+            programs.nixpkgs-fmt.enable = true;
+            programs.prettier.enable = true;
+            programs.taplo.enable = true;
+            programs.stylua.enable = true;
+            programs.beautysh = {
+              enable = true;
+              indent_size = 4;
+            };
+            projectRootFile = "flake.nix";
+            programs.nixpkgs-fmt.enable = true;
+            # Here you can specify the formatters to use
+            programs.terraform.enable = true;
+            # ...and options
+            programs.terraform.package = nixpkgs.terraform_1;
+          };
+      };}; 
+      };
 }
 
 #nixos-23.05
